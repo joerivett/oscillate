d7c8f9030ba0f51892726db33067c9982e9f996b
Joe Rivett <joe.rivett@songkick.com>
2019-11-02 22:51:43 +0000

    Fix issue where canvas controls consumed all document events
    This mean you couldn't interact with the range inputs as mouse
    move events were being caught elsewhere and default action
    prevented

--- src/view/SceneRenderer.ts
+++ src/view/SceneRenderer.ts
@@ -17,23 +17,26 @@ class SceneRenderer
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
 
-    document.getElementById('canvasContainer').appendChild(this.renderer.domElement);
+    let container = document.getElementById('canvasContainer');
+    container.appendChild(this.renderer.domElement);
 
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
 
-    this.camera.position.z = 3;
+    this.camera.position.z = 4;
+    this.camera.position.y = 4;
+    this.camera.position.x = 0;
 
     var dirLight = new THREE.DirectionalLight(0xffffff, 1);
     dirLight.position.set(0, 5, 5);
     this.scene.add(dirLight);
-
-	  this.controls = new THREE.TrackballControls( this.camera );
+    this.scene.add(this.camera);
+	  this.controls = new THREE.TrackballControls(this.camera, container);
   }
 
   update()
   {
-    this.renderer.render( this.scene, this.camera );
+    this.renderer.render(this.scene, this.camera);
     this.controls.update();
   }
 }
